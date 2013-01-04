dd74b40b8800ce2e3e269ead657d647ff399daa6
Claudio Brandolino <cbrandolino@gmail.com>
2013-01-04 19:07:13 +0100

    Main View-based routing, temporarily disabled authentication

--- js/app/AppRouter.js
+++ js/app/AppRouter.js
@@ -8,26 +8,19 @@ var AppRouter = Backbone.Router.extend({
   routes: {
     '': 'main',
     '!': 'main',
-    '!splash': 'splash'
-  },
-
-  // Check for soundcloud auth status.
-  // Redirect to splash page id user is not authenticated.
-  auth: function(callback) {
-    var self = this
-    //if (SC.isConnected())
-      callback()
-    //else
-    //  self.navigate("!splash", true)
+    '!:activeSection': 'main',
+    '!:activeSection/:subResource': 'main'
   },
 
   splash: function() {
     new App.views.splash({el: '#app'})
   },
 
-  main: function() {
-    this.auth(function(){
-      new App.views.main({el: '#app'})
+  main: function(activeSection, subResource) {
+    new App.views.main({
+      el: '#app',
+      activeSection: activeSection,
+      subResource: subResource
     })
   }
 })
--- js/app/views/Main.js
+++ js/app/views/Main.js
@@ -3,8 +3,9 @@ goog.require('Views.Search')
 goog.require('Views.Player')
 
 App.views.main = Backbone.View.extend({
-  initialize: function() {
-    this.activeSection = 'search'
+  initialize: function(options) {
+    this.activeSection = options.activeSection || 'search'
+    this.subResource = options.subResource || null
     this.render()
     // Instantiate the two global views, player and queue
     App.player = new App.views.player({el: this.$('.player')})
@@ -16,6 +17,9 @@ App.views.main = Backbone.View.extend({
     }
     var template = _.template($('#main').html(), templateData)
     this.$el.html(template)
-    this.view = new App.views[this.activeSection]({el: this.$('#section')})
+    this.view = new App.views[this.activeSection]({
+      el: this.$('#section'), 
+      options: {subresource: this.subResource}
+    })
   }
 })
